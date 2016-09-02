992d907a3713e979e5c7b00e39c71fda8278891d
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-01 19:51:43 -0700

    Added components for box, items, and form. Need logic on form submission

--- app/assets/javascripts/components/to_do_items.jsx
+++ app/assets/javascripts/components/to_do_items.jsx
@@ -57,11 +57,39 @@ var ToDoItem = React.createClass({
 });
 
 var ToDoItemForm = React.createClass({
+  getInitialState: function() {
+    return {title: '', description: ''};
+  },
+  handleTitleChange: function(e) {
+    this.setState({title: e.target.value});
+  },
+  handleDescriptionChange: function(e) {
+    this.setState({title: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = this.state.title.trim();
+    var description = this.state.description.trim();
+    if (!title || !description) {
+      return;
+    }
+    // TODO: send request to server
+  },
   render: function() {
     return (
-      <form className="toDoForm">
-        <input type="text" placeholder="Title" />
-        <input type="text" placeholder="Description" />
+      <form className="toDoForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text" 
+          placeholder="Title"
+          value={this.state.title}
+          onChange={this.handleTitleChange}
+        />
+        <input 
+          type="text" 
+          placeholder="Description"
+          value={this.state.description}
+          onChange={this.handleDescriptionChange}
+        />
         <input type="submit" placeholder="Post" />
       </form>
     )
