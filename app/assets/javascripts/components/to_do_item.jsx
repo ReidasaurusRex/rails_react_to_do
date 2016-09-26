d6e99ebf45d3046a9629016ab2f72cfb18d59fae
ReidasaurusRex <reidhlewis91@gmail.com>
2016-09-26 14:31:21 -0700

    Added a destroy button in style of react tutorial

--- app/assets/javascripts/components/to_do_item.jsx
+++ app/assets/javascripts/components/to_do_item.jsx
@@ -1,26 +1,14 @@
 var ToDoItem = React.createClass({
-  handleItemDestroy: function(id) {
-    console.log(id);
-    $.ajax({
-      url: "/to_do_items/" + id.id,
-      dataType: 'json',
-      type: 'DELETE',
-      data: id,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(ToDoBox),
-      error: function(xhr, status, err) {
-        console.log(this.props.url, status, err.toString());
-      }.bind(ToDoBox)
-      // TODO: fix binds
-    });
+  handleDelete: function(e) {
+    e.preventDefault();
+    this.props.onItemDestroy(this.props.id);
   },
   render: function() {
     return (
       <tr className="toDoItem">
         <td> {this.props.title} </td>
         <td> {this.props.description} </td>
-        <td><DestroyItemButton id={this.props.id} onItemDestroy={this.handleItemDestroy} /></td>
+        <td><a href="#" onClick= {this.handleDelete}>X</a></td>
       </tr>
     )
   }
--- app/assets/javascripts/components/to_do_items.jsx
+++ app/assets/javascripts/components/to_do_items.jsx
@@ -29,14 +29,27 @@ var ToDoBox = React.createClass({
       }.bind(this)
     });
   },
+  handleItemDestroy: function(id) {
+    $.ajax({
+      url: this.props.url + "/" + id,
+      type: 'DELETE',
+      data: id,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   componentDidMount: function() {
     this.loadItemsFromServer();
-    setInterval(this.loadItemsFromServer, this.props.pollInterval);
   },
   render: function() {
+    var self = this;
     var toDoItems = this.state.data.map(function(item) {
       return (
-        <ToDoItem title={item.title} description={item.description} key={item.id} id={item.id}/> 
+        <ToDoItem title={item.title} description={item.description} key={item.id} id={item.id} onItemDestroy={self.handleItemDestroy}/> 
       );
     });
     return (
